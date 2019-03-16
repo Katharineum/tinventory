db679735fb33d34ed400a6be0d8acf5c580b9590
HanseGucker <joniweth@gmx.de>
2019-03-16 17:43:55 +0100

    Add verbose names

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.models import Category, Location
+from api.models import Category, Location, Preset
 
 admin.site.register(Category)
 admin.site.register(Location)
+admin.site.register(Preset)
--- api/models.py
+++ api/models.py
@@ -2,18 +2,43 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Name")
 
     # icon = models.CharField()
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Kategorie"
+        verbose_name_plural = "Kategorien"
+
 
 class Location(models.Model):
-    name = models.CharField(max_length=100)
-    number = models.IntegerField(unique=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name="Name")
+    number = models.IntegerField(unique=True, blank=True, verbose_name="Schranknummer")
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Ort"
+        verbose_name_plural = "Orte"
+
+
+def default_preset():
+    return Category.objects.get_or_create(name="Sonstiges")
+
+
+class Preset(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Bezeichnung")
+    manufacturer = models.CharField(max_length=100, blank=True, verbose_name="Hersteller")
+    description = models.TextField(blank=True, verbose_name="Beschreibung")
+    category = models.ForeignKey(Category, models.SET_DEFAULT, default=default_preset, verbose_name="Kategorie")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Preset"
+        verbose_name_plural = "Presets"
