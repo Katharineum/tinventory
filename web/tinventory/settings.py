13097660b24edfd6891857767885892202b11ab4
Jonathan Weth <git@jonathanweth.de>
2020-07-03 17:50:32 +0200

    Fix translation system

--- web/tinventory/settings.py
+++ web/tinventory/settings.py
@@ -72,6 +72,7 @@ AUTHENTICATION_BACKENDS = (
 )
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
